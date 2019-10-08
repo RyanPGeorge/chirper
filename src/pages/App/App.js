4d0f691033a9d1f44f40eff5d6379da7bb8d1e69
Ryan George <ryangeorge@Ryans-MacBook-Pro.local>
2019-10-08 12:37:06 -0500

    sign up and login page

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,14 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import SignupPage from '../SignupPage/SignupPage';
+import LoginPage from '../LoginPage/LoginPage';
+import userService from '../../utils/userService';
+import NavBar from '../../components/NavBar/NavBar';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>Chirper: Coming Soon</p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: userService.getUser()
+    };
+  }
+
+  handleLogout = () => {
+    userService.logout();
+    this.setState({ user: null });
+  };
+
+  handleSignupOrLogin = () => {
+    this.setState({ user: userService.getUser() });
+  };
+
+  render() {
+    return (
+      <div>
+        <header className='header-footer'>Chriper: Coming Soon....</header>
+        <Switch>
+          <Route
+            exact
+            path='/'
+            render={() => (
+              <NavBar user={this.state.user} handleLogout={this.handleLogout} />
+            )}
+          />
+          <Route
+            exact
+            path='/signup'
+            render={({ history }) => (
+              <SignupPage
+                history={history}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/login'
+            render={({ history }) => (
+              <LoginPage
+                history={history}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+            )}
+          />
+          :
+          <Redirect to='/login' />
+          }/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
