334330310d941cdd55de8af7368c5636ef6b4ad9
Ryan George <ryangeorge@Ryans-MacBook-Pro.local>
2019-10-07 22:42:10 -0500

    i am stuck

---
+++ src/pages/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>Chirper: Coming Soon</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
